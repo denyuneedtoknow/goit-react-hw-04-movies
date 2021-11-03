b980a2c772c17b7d47d4877ff0fb8d42532c78df
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-11-03 22:17:26 +0200

    nav tweak

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,20 +1,18 @@
-import React from "react"
-import { NavLink } from "react-router-dom"
-import s from './Navigation.module.css'
-
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import s from "./Navigation.module.css";
 
 const Navigation = () => {
+  return (
+    <div className={s.navigation}>
+      <NavLink exact to="/" className={s.link} activeClassName={s.activeLink}>
+        Home
+      </NavLink>
+      <NavLink to="/Movies" className={s.link} activeClassName={s.activeLink}>
+        Search Movies
+      </NavLink>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h1 className={s.header}>Navigation</h1>
-            <NavLink exact to='/' className={s.link} activeClassName={s.activeLink}>Home</NavLink>
-            <NavLink to='/Movies' className={s.link} activeClassName={s.activeLink}>Search Movies</NavLink>
-        </div>
-
-    )
-}
-
-export default Navigation
-
+export default Navigation;
